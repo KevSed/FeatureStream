8b2921d52a086893d98f3f49ce62e4b969c29845
Kevin Sedlaczek <kevin.sedlaczek@tu-dortmund.de>
2017-12-11 17:04:06 +0100

    __main__ initailized

---
+++ features_from_phs/__main__.py
@@ -0,0 +1,16 @@
+from .gen_features import gen_features
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument(input_file)
+parser.add_argument(output_file)
+
+
+def main():
+    args = parser.parse_args()
+
+    print_progress(args.input_file, args.output_file)
+
+
+if __name__ == '__main__':
+    main()
