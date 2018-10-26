d246549ad8c616b55d2ef6f62af92339f4af0e55
Kevin Sedlaczek <kevin.sedlaczek@tu-dortmund.de>
2018-10-26 22:54:26 +0200

    Change name to feature_stream

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
-setup(name='features_from_phs',
+setup(name='feature_stream',
       version='0.1',
       description='Generate features for analysis from photon stream data',
       url='http://github.com/KevSed/features_from_phs',
       author='Kevin Sedlaczek',
       author_email='kevin.sedlaczek@tu-dortmund.de',
       license='GNU',
-      packages=['features_from_phs'],
+      packages=['feature_stream'],
       install_requires=[
           'numpy',
           'scipy',
